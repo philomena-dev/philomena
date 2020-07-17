8f1a953e28f0efa327a4d946345934c058c0e508
ZizzyDizzyMC <ZizzyDizzyMC@gmail.com>
2020-07-16 23:52:13 -0400

    Adding in processed filters.
    This should work...

--- lib/philomena/workers/thumbnail_worker.ex
+++ lib/philomena/workers/thumbnail_worker.ex
@@ -1,6 +1,7 @@
 defmodule Philomena.ThumbnailWorker do
   alias Philomena.Images.Thumbnailer
-
+  alias Philomena.Elasticsearch
+  alias Philomena.Images.Image
   def perform(image_id) do
     Thumbnailer.generate_thumbnails(image_id)
 
@@ -9,5 +10,6 @@ defmodule Philomena.ThumbnailWorker do
       "image:process",
       %{image_id: image_id}
     )
+    Elasticsearch.update_mapping!(Image)
   end
 end
