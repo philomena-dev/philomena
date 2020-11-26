494009e895c85f58adceb4c4b59f6fe0fb46aa04
Parasprite <foalspeedahead@gmail.com>
2020-11-26 14:22:52 -0600

    Updates user link to artist link (migration)
    bop

---
+++ priv/repo/migrations/20201124224116_rename_user_links_table.exs
@@ -0,0 +1,16 @@
+defmodule Philomena.Repo.Migrations.RenameUserLinksTable do
+  use Ecto.Migration
+
+  def change do
+    rename table("user_links"), to: table("artist_links")
+    execute "ALTER SEQUENCE user_links_id_seq RENAME TO artist_links_id_seq"
+    execute "ALTER INDEX index_user_links_on_aasm_state RENAME TO index_artist_links_on_aasm_state"
+    execute "ALTER INDEX index_user_links_on_contacted_by_user_id RENAME TO index_artist_links_on_contacted_by_user_id"
+    execute "ALTER INDEX index_user_links_on_next_check_at RENAME TO index_artist_links_on_next_check_at"
+    execute "ALTER INDEX index_user_links_on_tag_id RENAME TO index_artist_links_on_tag_id"
+    execute "ALTER INDEX index_user_links_on_uri_tag_id_user_id RENAME TO index_artist_links_on_uri_tag_id_user_id"
+    execute "ALTER INDEX index_user_links_on_user_id RENAME TO index_artist_links_on_user_id"
+    execute "ALTER INDEX index_user_links_on_verified_by_user_id RENAME TO index_artist_links_on_verified_by_user_id"
+    execute "UPDATE roles SET resource_type='ArtistLink' WHERE resource_type='UserLink'"
+  end
+end
