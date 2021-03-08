6ca4444d352cd42a5abc91e84d60790e9e4ed9df
ZizzyDizzyMC <ZizzyDizzyMC@gmail.com>
2021-03-08 16:26:40 -0500

    Update image_updater.ex

--- lib/philomena_web/image_updater.ex
+++ lib/philomena_web/image_updater.ex
@@ -26,38 +26,38 @@ defmodule PhilomenaWeb.ImageUpdater do
   
   defp run do
     # Read view counts from mailbox
-    views = receive_all()
+    views_count = receive_all()
 
     now = DateTime.utc_now() |> DateTime.truncate(:second)
 
     # Create insert statements for Ecto
-    views = Enum.map(views, &views_insert_all(&1, now))
+    views_count = Enum.map(views_count, &views_insert_all(&1, now))
 
     # Merge into table
-    views_update = update(Image, inc: [views: fragment("EXCLUDED.views")])
+    views_update = update(Image, inc: [views_count: fragment("EXCLUDED.views_count")])
 
-    Repo.insert_all(Image, views, on_conflict: views_update, conflict_target: [:id])
+    Repo.insert_all(Image, views_count, on_conflict: views_update, conflict_target: [:id])
 
     :timer.sleep(:timer.seconds(10))
 
     run()
   end
 
-  defp receive_all(views \\ %{}) do
+  defp receive_all(views_count \\ %{}) do
     receive do
       image_id ->
-        views = Map.update(views_count, image_id, 1, &(&1 + 1))
-        receive_all(views)
+        views_count = Map.update(views_count, image_id, 1, &(&1 + 1))
+        receive_all(views_count)
     after
       0 ->
-        views
+        views_count
     end
   end
 
-  defp views_insert_all({image_id, views}, now) do
+  defp views_insert_all({image_id, views_count}, now) do
     %{
       id: image_id,
-      views_count: views,
+      views_count: views_count,
       created_at: now,
       updated_at: now
     }
