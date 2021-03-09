f50ef0ebae1d86fd6c7dca1f8ef63f797b301c8d
ZizzyDizzyMC <ZizzyDizzyMC@gmail.com>
2021-03-08 20:43:17 -0500

    Update image_controller.ex

--- lib/philomena_web/controllers/image_controller.ex
+++ lib/philomena_web/controllers/image_controller.ex
@@ -168,7 +168,7 @@ defmodule PhilomenaWeb.ImageController do
         [i, _],
         _ in fragment("SELECT COUNT(*) FROM source_changes s WHERE s.image_id = ?", i.id)
       )
-      |> preload([:deleter, :view, :locked_tags, user: [awards: :badge], tags: :aliases])
+      |> preload([:deleter, :view, user: [awards: :badge], tags: :aliases])
       |> select([i, t, s], {i, t.count, s.count})
       |> Repo.one()
       |> case do
