cd4281fe619f9392c0e3efc22bd527f109acb25d
ZizzyDizzyMC <ZizzyDizzyMC@gmail.com>
2021-03-19 00:37:39 -0400

    Themes update.

--- lib/philomena_web/views/layout_view.ex
+++ lib/philomena_web/views/layout_view.ex
@@ -73,8 +73,26 @@ defmodule PhilomenaWeb.LayoutView do
   def stylesheet_path(conn, %{theme: "olddefault"}),
     do: Routes.static_path(conn, "/css/olddefault.css")
 
-  def stylesheet_path(conn, %{theme: "ponerpics"}),
-    do: Routes.static_path(conn, "/css/ponerpics.css")
+  def stylesheet_path(conn, %{theme: "ponerpics-default"}),
+    do: Routes.static_path(conn, "/css/ponerpics-default.css")
+
+  def stylesheet_path(conn, %{theme: "manebooru-default"}),
+    do: Routes.static_path(conn, "/css/manebooru-default.css")
+
+  def stylesheet_path(conn, %{theme: "manebooru-fuchsia"}),
+    do: Routes.static_path(conn, "/css/manebooru-fuchsia.css")
+
+  def stylesheet_path(conn, %{theme: "manebooru-green"}),
+    do: Routes.static_path(conn, "/css/manebooru-green.css")
+
+  def stylesheet_path(conn, %{theme: "manebooru-orange"}),
+    do: Routes.static_path(conn, "/css/manebooru-orange.css")
+
+  def stylesheet_path(conn, %{theme: "twibooru-default"}),
+    do: Routes.static_path(conn, "/css/twibooru-default.css")
+	
+  def stylesheet_path(conn, %{theme: "furbooru-default"}),
+    do: Routes.static_path(conn, "/css/furbooru-default.css")
     
   def stylesheet_path(conn, _user),
     do: Routes.static_path(conn, "/css/default.css")
