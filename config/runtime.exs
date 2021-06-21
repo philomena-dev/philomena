c2005fee5c938349b352e133dbc939e0230ba1ff
ZizzyDizzyMC <ZizzyDizzyMC@gmail.com>
2021-06-21 03:22:36 -0400

    Update runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -35,8 +35,8 @@ config :philomena,
   camo_host: System.get_env("CAMO_HOST"),
   camo_key: System.get_env("CAMO_KEY"),
   cdn_host: System.fetch_env!("CDN_HOST"),
-  booru_name: System.fetch_env!("BOORU_NAME", "Philomena"),
-  booru_tagline: System.fetch_env!("BOORU_TAGLINE", "Philomena is a linear imagebooru which lets you share, find and discover new art and media within the MLP Fandom")
+  booru_name: System.get_env("BOORU_NAME", "Philomena"),
+  booru_tagline: System.get_env("BOORU_TAGLINE", "Philomena is a linear imagebooru which lets you share, find and discover new art and media within the MLP Fandom")
 app_dir = System.get_env("APP_DIR", File.cwd!())
 
 json_config =
