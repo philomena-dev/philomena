15f74647852755fb136ca117946c16385d1eb467
erhannis <eyeillus@gmail.com>
2021-09-23 23:05:30 -0400

    Ok, I THINK I have the migration correct, now

--- priv/repo/migrations/20210921025336_change_image_scale_option_type.exs
+++ priv/repo/migrations/20210921025336_change_image_scale_option_type.exs
@@ -1,9 +1,20 @@
 defmodule Philomena.Repo.Migrations.ChangeImageScaleOptionType do
   use Ecto.Migration
+  import Ecto.Query
 
   def change do
     alter table(:users) do
-      modify :scale_large_images, :binary, default: "true"
+      add :scale_large_images0, :binary, default: "true", null: false
     end
+  
+    flush() # Not sure if this is necessary
+  
+    execute("update users set scale_large_images0 = (CASE WHEN scale_large_images THEN 'true'::bytea ELSE 'false'::bytea END) where scale_large_images is not null;")
+  
+    alter table(:users) do
+      remove :scale_large_images
+    end
+
+    rename table(:users), :scale_large_images0, to: :scale_large_images
   end
 end
