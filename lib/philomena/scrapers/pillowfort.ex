efef123fabfc3f26de12a51c4df9ff8a7348986e
byte[] <byteslice@airmail.cc>
2021-12-19 22:30:01 -0500

    More polished Pillowfort scraper

--- lib/philomena/scrapers/pillowfort.ex
+++ lib/philomena/scrapers/pillowfort.ex
@@ -1,16 +1,15 @@
 defmodule Philomena.Scrapers.Pillowfort do
-  @url_regex ~r|\Ahttps?://[-a-z0-9.]+.pillowfort\.social/posts/[0-9]+\z| # probly'd fail for https://pillowfort...
-  @post_regex ~r|\Ahttps?://[-a-z0-9.]+.pillowfort\.social/posts/([0-9]+)\z|
+  @url_regex ~r|\Ahttps?://www\.pillowfort\.social/posts/([0-9]+)|
 
-  @spec can_handle?(URI.t(), String.t()) :: true | false
+  @spec can_handle?(URI.t(), String.t()) :: boolean()
   def can_handle?(_uri, url) do
     String.match?(url, @url_regex)
   end
 
-  def scrape(uri, url) do
-    [post_id] = Regex.run(@post_regex, url, capture: :all_but_first)
+  def scrape(_uri, url) do
+    [post_id] = Regex.run(@url_regex, url, capture: :all_but_first)
 
-    api_url = "#{uri.scheme}://#{uri.host}/posts/#{post_id}/json"
+    api_url = "https://www.pillowfort.social/posts/#{post_id}/json"
 
     Philomena.Http.get(api_url)
     |> json!()
@@ -20,22 +19,32 @@ defmodule Philomena.Scrapers.Pillowfort do
   defp json!({:ok, %Tesla.Env{body: body, status: 200}}),
     do: Jason.decode!(body)
 
-  defp process_response!(postJson, url) do
+  defp process_response!(post_json, url) do
     images =
-      postJson["media"]
+      post_json["media"]
       |> Enum.map(
         &%{
           url: &1["url"],
-          camo_url: Camo.Image.image_url(&1["url"])
+          camo_url: Camo.Image.image_url(&1["small_image_url"])
         }
       )
-    
+
     %{
       source_url: url,
-      author_name: postJson["username"],
-      description: postJson["title"] <> "\n----\n" <> postJson["content"],
+      author_name: post_json["username"],
+      description: Enum.join(title(post_json) ++ content(post_json), "\n\n---\n\n"),
       images: images
     }
   end
-  
+
+  defp title(%{"title" => title}) when title not in [nil, ""], do: [remove_html_tags(title)]
+  defp title(_), do: []
+
+  defp content(%{"content" => content}) when content not in [nil, ""], do: [remove_html_tags(content)]
+  defp content(_), do: []
+
+  defp remove_html_tags(text) do
+    # The markup parser won't render these tags, so remove them
+    String.replace(text, ~r|<.+?>|, "")
+  end
 end
