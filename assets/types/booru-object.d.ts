fd289b6a63c3c25ec731e78409b91a2c58ec98fa
SeinopSys <seinopsys@protonmail.com>
2021-12-28 01:45:11 +0100

    expand BooruObject type with additional scouted keys for later use

--- assets/types/booru-object.d.ts
+++ assets/types/booru-object.d.ts
@@ -2,7 +2,35 @@ type SpoilerType = 'click' | 'hover' | 'off';
 
 interface BooruObject {
   csrfToken: string;
+  /**
+   * One of the specified values, based on user setting
+   */
   spoilerType: SpoilerType;
+  /**
+   * List of numeric image IDs as strings
+   */
+  imagesWithDownvotingDisabled: string[];
+  /**
+   * Array of watched tag IDs as numbers
+   */
+  watchedTagList: number[];
+  /**
+   * Array of spoilered tag IDs as numbers
+   */
+  spoileredTagList: number[];
+  /**
+   * Array of hidden tag IDs as numbers
+   */
+  hiddenTagList: number[];
+  /**
+   * Stores the URL of the default "tag blocked" image
+   */
+  hiddenTag: string;
+  userIsSignedIn: boolean;
+  /**
+   * Indicates if the current user has edit rights to the currently selected filter
+   */
+  userCanEditFilter: boolean;
 }
 
 interface Window {
