7841bbd2b2588dec669ee595cd1750f857de5e67
SeinopSys <seinopsys@protonmail.com>
2022-01-23 14:50:26 +0100

    upgrade testing dependencies and fix window.booru initial setup value

--- assets/test/jest-setup.ts
+++ assets/test/jest-setup.ts
@@ -3,4 +3,11 @@ import '@testing-library/jest-dom';
 window.booru = {
   csrfToken: 'mockCsrfToken',
   spoilerType: 'off',
+  userIsSignedIn: false,
+  hiddenTag: '/mock-tagblocked.svg',
+  hiddenTagList: [],
+  imagesWithDownvotingDisabled: [],
+  spoileredTagList: [],
+  userCanEditFilter: false,
+  watchedTagList: [],
 };
