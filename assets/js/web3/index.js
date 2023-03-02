201b32498770508dc845617527d41f1014460974
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-02 20:13:07 -0300

    fix

--- assets/js/web3/index.js
+++ assets/js/web3/index.js
@@ -6,13 +6,13 @@ const startWeb3 = function() {
   // Check if Web3 has been injected by the browser (Mist/MetaMask).
   window.web3 = web3;
   if (typeof ethereum !== 'undefined') {
-    window.web3Provider =  new Web3(window.ethereum);
+    window.web3Provider = new Web3(window.ethereum);
   }
 
   // Detect Connect Wallet Buttom
   const connectWallet = $('#connect-web3-wallet');
-  if (connectWallet) { 
-    
+  if (connectWallet) {
+
   }
 
 };
