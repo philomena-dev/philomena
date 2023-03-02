dc49f8fc562bf73ffcd5ab0e3f5d479d246f587e
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-02 20:38:36 -0300

    test connect web3 wallet click buttom

--- assets/js/web3/index.js
+++ assets/js/web3/index.js
@@ -1,20 +1,29 @@
+// More Modules
 import { $ } from '../utils/dom';
 import * as web3 from 'web3';
 
+// Module
 const startWeb3 = function() {
 
   // Check if Web3 has been injected by the browser (Mist/MetaMask).
   window.web3 = web3;
   if (typeof ethereum !== 'undefined') {
-    window.web3Provider = new Web3(window.ethereum);
+    window.tinyCrypto = { provider: new Web3(window.ethereum) };
   }
+
+  // Page Load Ready
 
   // Detect Connect Wallet Buttom
   const connectWallet = $('#connect-web3-wallet');
   if (connectWallet) {
+    connectWallet.addEventListener('click', () => {
 
+      console.log('Test Wallet Buttom');
+
+    });
   }
 
 };
 
+// Export Module
 export { startWeb3 };
