ff19c83f0de2bd4c8f9c504a6bb48823a538b1e7
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-02 18:04:00 -0300

    web3 module added to the package assets

--- assets/js/web3/index.js
+++ assets/js/web3/index.js
@@ -1,24 +1,18 @@
-import { $ } from './utils/dom';
+import { $ } from '../utils/dom';
+import * as web3 from 'web3';
 
 const startWeb3 = function() {
 
   // Check if Web3 has been injected by the browser (Mist/MetaMask).
-  if (typeof web3 !== 'undefined') {
-    console.log('Web3 installed.');
-  } else {
-    console.log('No Web3 installed.');
-  }
-
+  window.web3 = web3;
   if (typeof ethereum !== 'undefined') {
-    console.log('Ethereum installed.');
-  } else {
-    console.log('No Ethereum installed.');
+    window.web3Provider =  new Web3(window.ethereum);
   }
 
   // Detect Connect Wallet Buttom
   const connectWallet = $('#connect-web3-wallet');
   if (connectWallet) { 
-    w
+    
   }
 
 };
