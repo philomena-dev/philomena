90dbaff62ab5862ede67e283ac59d4b399740a6e
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-02 21:05:16 -0300

    enable and disable web3 javascript added

--- web3/client.side.config.js
+++ web3/client.side.config.js
@@ -3,8 +3,14 @@ const web3Cfg = function() {
 
     return {
 
+        // Load Web3 Javascript
+        enabled: true,
+
         // Use the Polygon blockchain on the web3 provider. If you remove this setting, you will use the default option (Ethereum).
-        network: 'matic'
+        network: 'matic',
+
+        // Force Network Change
+        forceChange: false
     
     };
 
