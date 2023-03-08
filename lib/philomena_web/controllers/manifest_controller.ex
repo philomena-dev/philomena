6ff9bd2da74c0bbb326f9c89a9cd44663e88d630
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-08 01:27:24 -0300

    manifest test

--- lib/philomena_web/controllers/manifest_controller.ex
+++ lib/philomena_web/controllers/manifest_controller.ex
@@ -16,7 +16,58 @@ defmodule PhilomenaWeb.ManifestController do
       gcm_sender_id: "",
       display: "standalone",
 
-      icons: Arrays.new(["Dvorak", "Tchaikovsky", "Bruch"])
+      icons: [
+        %{
+            src: "/icon/16.png",
+            sizes: "16x16",
+            type: "image/png"
+        },
+        %{
+            src: "/icon/24.png",
+            sizes: "24x24",
+            type: "image/png"
+        },
+        %{
+            src: "/icon/32.png",
+            sizes: "32x32",
+            type: "image/png"
+        },
+        %{
+            src: "/icon/48.png",
+            sizes: "48x48",
+            type: "image/png"
+        },
+        %{
+            src: "/icon/64.png",
+            sizes: "64x64",
+            type: "image/png"
+        },
+        %{
+            src: "/icon/72.png",
+            sizes: "72x72",
+            type: "image/png"
+        },
+        %{
+            src: "/icon/80.png",
+            sizes: "80x80",
+            type: "image/png"
+        },
+        %{
+            src: "/icon/96.png",
+            sizes: "96x96",
+            type: "image/png"
+        },
+        %{
+            src: "/icon/128.png",
+            sizes: "128x128",
+            type: "image/png"
+        },
+        %{
+            src: "/icon/256.png",
+            sizes: "256x256",
+            type: "image/png"
+        }
+      ]
 
     }
 
