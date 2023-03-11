6e0dd41e8f4e664d449b5f9144ab79840739ac6f
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-11 17:56:41 -0300

    web3 signer message fixed

--- lib/philomena_web/controllers/web3/sign_controller.ex
+++ lib/philomena_web/controllers/web3/sign_controller.ex
@@ -8,7 +8,7 @@ defmodule PhilomenaWeb.Web3SignerController do
       name: conn.assigns.current_user.name,
       id: conn.assigns.current_user.id,
 
-      desc: "Hello Crypto Brony, welcome to Derpibooru! We need your signture to confirm your web3 identity into your account.\n\nUsername: {name}\nId: {id}"
+      desc: "Hello Crypto Brony, welcome to Derpibooru! We need your signture to confirm your web3 identity into your account.\n\nUsername: #{conn.assigns.current_user.name}\nId: #{conn.assigns.current_user.id}"
 
     }
 
