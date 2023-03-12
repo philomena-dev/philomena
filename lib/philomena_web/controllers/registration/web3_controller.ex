e0e77031b716fde5a29b851954dfe9edc3d40073
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-12 04:23:48 -0300

    preparing test files for the web3 database

--- lib/philomena_web/controllers/registration/web3_controller.ex
+++ lib/philomena_web/controllers/registration/web3_controller.ex
@@ -2,18 +2,27 @@ defmodule PhilomenaWeb.Registration.Web3Controller do
   use PhilomenaWeb, :controller
 
   alias Philomena.Search.Parser
-  alias Philomena.Users
+  alias Philomena.Web3
   alias Philomena.Repo
+  alias PhilomenaWeb.Web3SignerData
 
   plug PhilomenaWeb.FilterBannedUsersPlug
 
   def edit(conn, params) do
-    changeset = Users.change_user(conn.assigns.current_user)
+    changeset = Web3.change_address(conn.assigns.current_user)
     render(conn, "edit.html", title: "Editing Web3 Account", changeset: changeset, current_user: conn.assigns.current_user)
   end
 
   def update(conn, %{"user" => user_params}) do
+    case Web3.update_address(conn.assigns.current_user, user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Web3 Address successfully updated.")
+        |> redirect(to: Routes.registration_web3_path(conn, :show, user))
 
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset)
+    end
   end
 
 end
