d6269841b235500df6e1138438afc66b4b6a394f
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-13 18:08:11 -0300

    web3 migration file

--- priv/repo/migrations/20230313183010_add_web3_tables.exs
+++ priv/repo/migrations/20230313183010_add_web3_tables.exs
@@ -3,5 +3,41 @@ defmodule Philomena.Repo.Migrations.AddWeb3Tables do
 
   def change do
 
+    alter table("users") do
+      add :ethereum, :varchar, null: false
+    end
+
+    execute(
+      "CREATE TABLE public.ethereum_changes (
+        id integer NOT NULL,
+        user_id bigint NOT NULL,
+        ethereum character varying NOT NULL,
+        sign_data character varying NOT NULL,
+        created_at timestamp without time zone NOT NULL,
+        updated_at timestamp without time zone NOT NULL
+    )"
+    )
+
+    execute(
+      "CREATE SEQUENCE public.ethereum_changes_id_seq
+      START WITH 1
+      INCREMENT BY 1
+      NO MINVALUE
+      NO MAXVALUE
+      CACHE 1;"
+    )
+
+    execute(
+      "ALTER TABLE ONLY public.ethereum_changes ALTER COLUMN id SET DEFAULT nextval('public.ethereum_changes_id_seq'::regclass);"
+    )
+
+    execute(
+      "ALTER TABLE ONLY public.ethereum_changes ADD CONSTRAINT ethereum_changes_pkey PRIMARY KEY (id);"
+    )
+
+    execute(
+      "CREATE INDEX index_ethereum_changes_on_user_id ON public.ethereum_changes USING btree (user_id);"
+    )
+
   end
 end
