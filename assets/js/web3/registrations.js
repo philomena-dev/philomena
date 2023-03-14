939fddaaad95b7f55ee1215aaaef1e92f21aacad
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-13 21:22:03 -0300

    web3 js file buttom update

--- assets/js/web3/registrations.js
+++ assets/js/web3/registrations.js
@@ -5,24 +5,37 @@ const configWeb3 = function() {
   // Detect Connect Wallet Buttom
   const connectWallet = $('#connect-web3-wallet');
   if (connectWallet) {
-    connectWallet.addEventListener('click', () => {
+    if (window.ethereum) {
+      connectWallet.addEventListener('click', () => {
+        if (window.ethereum._isUnlocked) {
 
-      fetch('/registrations/web3/sign', {
-        headers: {
-          'Content-Type': 'application/json',
+          fetch('/registrations/web3/sign', {
+            headers: {
+              'Content-Type': 'application/json',
+            }
+          })
+            .then(response => response.json())
+
+            .then(data => {
+              window.tinyCrypto.call.sign(data.desc, '');
+            })
+
+            .catch(err => {
+              console.error(err);
+              alert(err.message);
+            });
+
         }
-      })
-        .then(response => response.json())
 
-        .then(data => {
-          window.tinyCrypto.call.sign(data.desc, '');
-        })
+        else {
+          alert('Please unlock your crypto wallet before using this.');
+        }
 
-        .catch(err => {
-          console.error(err);
-        });
-
-    });
+      });
+    }
+    else {
+      connectWallet.attr('disabled', true);
+    }
   }
 
 };
