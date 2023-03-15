afadb48046b691ac8112c2056f12774261c264f8
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-14 22:53:15 -0300

    ContextMenu test

--- assets/js/web3/profile.js
+++ assets/js/web3/profile.js
@@ -6,10 +6,33 @@ const profileWeb3 = function() {
     const address = $('#web3_profile_data #address').innerText.trim();
     for (const network in window.tinyCrypto.config.networks) {
 
+      // Create Div
       const contentDiv = document.createElement('div');
-      contentDiv.innerHTML = `<a href="${window.tinyCrypto.config.networks[network].blockExplorerUrls[0]}address/${address}" target="_blank">${window.tinyCrypto.config.networks[network].chainName}</a>`;
-
+      contentDiv.innerHTML = `<a id="pf-crypto-menu-${network}" href="${window.tinyCrypto.config.networks[network].blockExplorerUrls[0]}address/${address}" target="_blank">${window.tinyCrypto.config.networks[network].chainName}</a>`;
       profileHeadBase.appendChild(contentDiv);
+
+      // eslint-disable-next-line no-undef
+      const myMenu = new ContextMenu({
+        target: `#pf-crypto-menu-${network}`,
+        menuItems: [
+          {
+            content: 'Item 1',
+            events: {
+              click: e => console.log(e, 'Copy Button Click')
+              // You can use any event listener from here
+            }
+          },
+          { content: 'Item 2' },
+          { content: 'Item 3' },
+          { content: 'Item 4' },
+          {
+            content: 'Item 5',
+            divider: 'top' // top, bottom, top-bottom
+          }
+        ]
+      });
+
+      myMenu.init();
 
     }
   }
