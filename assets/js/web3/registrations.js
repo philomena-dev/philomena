a5ffd9aaa5346b118c09cfc6cc17a512c11def6a
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-15 01:47:09 -0300

    connect wallet web3 button fixed

--- assets/js/web3/registrations.js
+++ assets/js/web3/registrations.js
@@ -18,42 +18,36 @@ const configWeb3 = function() {
   const connectWallet = $('#connect-web3-wallet');
   if (connectWallet) {
     if (window.ethereum) {
-      if (window.ethereum._isUnlocked) {
-        connectWallet.addEventListener('click', () => {
-          if (window.ethereum._isUnlocked) {
+      connectWallet.addEventListener('click', () => {
+        if (window.ethereum._isUnlocked) {
 
-            fetch('/registrations/web3/sign', {
-              headers: {
-                'Content-Type': 'application/json',
-              }
-            })
-              .then(response => response.json())
+          fetch('/registrations/web3/sign', {
+            headers: {
+              'Content-Type': 'application/json',
+            }
+          })
+            .then(response => response.json())
 
-              .then(data => {
-                window.tinyCrypto.call.sign(data.desc, '').then(signature => {
-                  $('#web3_signature').setAttribute('value', signature);
-                  $('#web3_wallet').setAttribute('value', window.tinyCrypto.address);
-                  $('form[action="/registrations/web3"]').submit();
-                });
-              })
-
-              .catch(err => {
-                console.error(err);
-                alert(err.message);
+            .then(data => {
+              window.tinyCrypto.call.sign(data.desc, '').then(signature => {
+                $('#web3_signature').setAttribute('value', signature);
+                $('#web3_wallet').setAttribute('value', window.tinyCrypto.address);
+                $('form[action="/registrations/web3"]').submit();
               });
+            })
 
-          }
+            .catch(err => {
+              console.error(err);
+              alert(err.message);
+            });
 
-          else {
-            alert('Please unlock your crypto wallet before using this.');
-          }
+        }
 
-        });
-      }
-      else {
-        connectWallet.innerHTML = '<i class="fab fa-ethereum"></i> Please unlock your crypto wallet';
-        connectWallet.setAttribute('disabled', true);
-      }
+        else {
+          alert('Please unlock your crypto wallet before using this.');
+        }
+
+      });
     }
     else {
       connectWallet.innerHTML = '<i class="fab fa-ethereum"></i> You don\'t have a Web3 Wallet installed in your browser!';
