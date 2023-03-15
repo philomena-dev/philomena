24b2a13d74b76cad98a3f1545b829dc2ed69f42e
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-15 04:06:28 -0300

    web3/config.ex file

---
+++ web3/config.ex
@@ -0,0 +1,14 @@
+# If you are afraid of web3. Disable all settings here.
+# I don't recommend you activate the NFT option unless you are ready to take on this kind of responsibility. Find more information in the repository docs before actually activating it.
+defmodule PhilomenaWeb.Web3Cfg do
+  use PhilomenaWeb, :controller
+
+  def get() do
+    %{
+      enable_profile: true,
+      enable_comissions: true,
+      enable_nft: false
+    }
+  end
+
+end
