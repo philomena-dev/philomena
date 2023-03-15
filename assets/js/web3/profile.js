067c5456fc1b1afab18edf666d51c9dd76052b39
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-14 21:24:38 -0300

    preparing profile urls

--- assets/js/web3/profile.js
+++ assets/js/web3/profile.js
@@ -1,9 +1,18 @@
 // Module
 import { $ } from '../utils/dom';
 const profileWeb3 = function() {
+  const profileHeadBase = $('#web3_profile_data');
+  if (profileHeadBase) {
+    const address = $('#web3_profile_data #address').innerText.trim();
+    for (const network in window.tinyCrypto.config.networks) {
 
+      const contentDiv = document.createElement('div');
+      contentDiv.innerHTML = `<a href="${window.tinyCrypto.config.networks[network].chainName.blockExplorerUrls[0]}address/${address}" target="_blank">${window.tinyCrypto.config.networks[network].chainName}</a>`;
 
+      profileHeadBase.appendChild(contentDiv);
 
+    }
+  }
 };
 
 export { profileWeb3 };
