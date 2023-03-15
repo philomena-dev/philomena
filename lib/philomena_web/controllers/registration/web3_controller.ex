27075b63256107cf4dfc36fd30532aa2dbd2e799
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-15 04:27:32 -0300

    web3 config

--- lib/philomena_web/controllers/registration/web3_controller.ex
+++ lib/philomena_web/controllers/registration/web3_controller.ex
@@ -2,22 +2,33 @@ defmodule PhilomenaWeb.Registration.Web3Controller do
   use PhilomenaWeb, :controller
 
   alias Philomena.Web3
+  alias PhilomenaWeb.Web3Cfg
   plug PhilomenaWeb.FilterBannedUsersPlug
 
   def edit(conn, params) do
-    changeset = Web3.change_address(conn.assigns.current_user)
-    render(conn, "edit.html", title: "Editing Web3 Account", changeset: changeset, current_user: conn.assigns.current_user)
+    tinyWeb3Cfg = Web3Cfg.get()
+    if tinyWeb3Cfg.enable_profile do
+      changeset = Web3.change_address(conn.assigns.current_user)
+      render(conn, "edit.html", title: "Editing Web3 Account", changeset: changeset, current_user: conn.assigns.current_user)
+    else
+
+    end
   end
 
   def update(conn, %{"user" => user_params}) do
-    case Web3.update_address(conn.assigns.current_user, user_params) do
-      {:ok, user} ->
-        conn
-        |> put_flash(:info, "Web3 Address successfully updated.")
-        |> redirect(to: Routes.profile_path(conn, :show, user))
+    tinyWeb3Cfg = Web3Cfg.get()
+    if tinyWeb3Cfg.enable_profile do
+      case Web3.update_address(conn.assigns.current_user, user_params) do
+        {:ok, user} ->
+          conn
+          |> put_flash(:info, "Web3 Address successfully updated.")
+          |> redirect(to: Routes.profile_path(conn, :show, user))
 
-      {:error, changeset} ->
-        render(conn, "edit.html", changeset: changeset)
+        {:error, changeset} ->
+          render(conn, "edit.html", changeset: changeset)
+      end
+    else
+
     end
   end
 
