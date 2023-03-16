737bb98b42e2edcc9453aba913d5200a21769094
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-16 01:27:22 -0300

    price edit. web3 added. preparing more functions

--- lib/philomena_web/views/profile/commission/item_view.ex
+++ lib/philomena_web/views/profile/commission/item_view.ex
@@ -2,6 +2,10 @@ defmodule PhilomenaWeb.Profile.Commission.ItemView do
   use PhilomenaWeb, :view
 
   alias Philomena.Commissions.Commission
+  alias PhilomenaWeb.Web3Cfg
 
   def types, do: Commission.types()
+  def currencies, do: Web3Cfg.currencies()
+  def currencies_type, do: Web3Cfg.currenciesType()
+  def web3Cfg, do: Web3Cfg.get()
 end
