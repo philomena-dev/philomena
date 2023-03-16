fdc9314e8ab797ce36fc2153dcaf691311c3c1f2
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-15 21:56:00 -0300

    web3Config.ex - more new configs added

--- lib/philomena_web/web3Config.ex
+++ lib/philomena_web/web3Config.ex
@@ -5,9 +5,32 @@ defmodule PhilomenaWeb.Web3Cfg do
 
   def get() do
     %{
+
       enable_profile: true,
       enable_comissions: true,
-      enable_nft: false
+      enable_nft: false,
+
+      currencies: %{
+        polygon: "MATIC",
+        ethereum: "ETH",
+        bsc: "BNB"
+      },
+
+      usd: %{
+
+        usdt: %{
+          ethereum: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+          polygon: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f"
+        },
+
+        usdc: %{
+          ethereum: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+          polygon: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
+          bsc: "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d"
+        }
+
+      }
+
     }
   end
 
