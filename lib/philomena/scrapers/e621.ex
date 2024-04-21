0fd45b33f72b2662a63148e7f2ba891be0a77303
Chaska <166928710+chaskayote@users.noreply.github.com>
2024-04-21 16:27:57 -0500

    adding e621 support
    Thanks baer!

---
+++ lib/philomena/scrapers/e621.ex
@@ -0,0 +1,26 @@
+defmodule Philomena.Scrapers.E621 do
+  @url_regex ~r|\Ahttps?://e621\.net/posts/([0-9]+)|
+
+  @spec can_handle?(URI.t(), String.t()) :: true | false
+  def can_handle?(_uri, url) do
+    String.match?(url, @url_regex)
+  end
+
+  def scrape(_uri, url) do
+    api_url = "#{url}.json"
+    {:ok, %Tesla.Env{status: 200, body: body}} = Philomena.Http.get(api_url)
+
+    json = Jason.decode!(body)
+    [submission] = json["post"]
+
+    %{
+      source_url: url,
+      author_name: Enum.join(submission["tags"]["artist"], ", "),
+      description: submission["description"],
+      images: [%{
+        url: "#{submission["file"]["url"]}",
+        camo_url: Camo.Image.image_url(submission["file"]["url"])
+      }]
+    }
+  end
+end
