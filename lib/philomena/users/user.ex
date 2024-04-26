04a5637fb8228b149f079ebbadece14e10b48c74
Chaska <166928710+chaskayote@users.noreply.github.com>
2024-04-26 13:09:48 -0500

    undoing webp messup

--- lib/philomena/users/user.ex
+++ lib/philomena/users/user.ex
@@ -410,7 +410,7 @@ defmodule Philomena.Users.User do
     |> validate_number(:avatar_size, greater_than: 0, less_than_or_equal_to: 300_000)
     |> validate_number(:avatar_width, greater_than: 0, less_than_or_equal_to: 1000)
     |> validate_number(:avatar_height, greater_than: 0, less_than_or_equal_to: 1000)
-    |> validate_inclusion(:avatar_mime_type, ~W(image/gif image/jpeg image/png image/webp))
+    |> validate_inclusion(:avatar_mime_type, ~W(image/gif image/jpeg image/png))
   end
 
   def remove_avatar_changeset(user) do
