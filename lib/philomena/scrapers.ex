d49972663608553ad971e6b2028cc071f3cca7ca
Chaska <166928710+chaskayote@users.noreply.github.com>
2024-04-29 00:21:17 -0500

    Adding Derpibooru

--- lib/philomena/scrapers.ex
+++ lib/philomena/scrapers.ex
@@ -8,6 +8,7 @@ defmodule Philomena.Scrapers do
     Philomena.Scrapers.E621,
     Philomena.Scrapers.Furaffinity,
     Philomena.Scrapers.Pixiv,
+    Philomena.SCrapers.Derpibooru,
     Philomena.Scrapers.Raw
   ]
 
