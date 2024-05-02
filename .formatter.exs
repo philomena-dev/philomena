88a28e646d0c8313973466c4dcd77e6c17f4a1ef
Liam <byteslice@airmail.cc>
2024-05-01 23:31:39 -0400

    Add converter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
 [
+  heex_line_length: 300,
   import_deps: [:ecto, :phoenix],
   inputs: ["*.{heex,ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
   plugins: [Phoenix.LiveView.HTMLFormatter],
