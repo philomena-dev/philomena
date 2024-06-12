2fc9a18f9fdb709a158819405a5e4accbd3502ab
Luna D. <luna@nighty.cloud>
2024-06-12 20:50:46 +0200

    editor styling + media list styling

--- lib/philomena_web/views/app_view.ex
+++ lib/philomena_web/views/app_view.ex
@@ -258,4 +258,9 @@ defmodule PhilomenaWeb.AppView do
   def get_flash(_, _key), do: %{}
 
   def site_name, do: gettext("Philomena Site")
+
+  def shorten_number(n) when n >= 1_000_000_000, do: "#{Float.floor(n / 1_000_000_000, 1)}B"
+  def shorten_number(n) when n >= 1_000_000, do: "#{Float.floor(n / 1_000_000, 1)}M"
+  def shorten_number(n) when n >= 1_000, do: "#{Float.floor(n / 1_000, 1)}K"
+  def shorten_number(n), do: n
 end
