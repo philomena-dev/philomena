b107330e2a8ad63c333c845cabd16b02798f9177
Luna D. <luna@nighty.cloud>
2024-09-30 22:32:09 +0200

    changes

--- assets/js/shortcuts.ts
+++ assets/js/shortcuts.ts
@@ -47,31 +47,16 @@ function isOK(event: KeyboardEvent): boolean {
   );
 }
 
+/* eslint-disable prettier/prettier */
 const keyCodes: ShortcutKeyMap = {
-  j() {
-    click('.js-prev');
-  }, // J - go to previous image
-  i() {
-    click('.js-up');
-  }, // I - go to index page
-  k() {
-    click('.js-next');
-  }, // K - go to next image
-  r() {
-    click('.js-rand');
-  }, // R - go to random image
-  s() {
-    click('.js-source-link');
-  }, // S - go to image source
-  l() {
-    click('.js-tag-sauce-toggle');
-  }, // L - edit tags
-  o() {
-    openFullView();
-  }, // O - open original
-  v() {
-    openFullViewNewTab();
-  }, // V - open original in a new tab
+  j() { click('.js-prev'); },             // J - go to previous image
+  i() { click('.js-up'); },               // I - go to index page
+  k() { click('.js-next'); },             // K - go to next image
+  r() { click('.js-rand'); },             // R - go to random image
+  s() { click('.js-source-link'); },      // S - go to image source
+  l() { click('.js-tag-sauce-toggle'); }, // L - edit tags
+  o() { openFullView(); },                // O - open original
+  v() { openFullViewNewTab(); },          // V - open original in a new tab
   f() {
     // F - favourite image
     click(getHover() ? `a.interaction--fave[data-image-id="${getHover()}"]` : '.block__header a.interaction--fave');
@@ -81,6 +66,7 @@ const keyCodes: ShortcutKeyMap = {
     click(getHover() ? `a.interaction--upvote[data-image-id="${getHover()}"]` : '.block__header a.interaction--upvote');
   },
 };
+/* eslint-enable prettier/prettier */
 
 export function listenForKeys() {
   document.addEventListener('keydown', (event: KeyboardEvent) => {
