771c7de9bba59c3f2f9efb5f72c33bb1579ba596
mdashlw <mdashlw@gmail.com>
2025-02-11 06:27:54 +0000

    use can? :show image in Json.ImageView

--- lib/philomena_web/views/api/json/image_view.ex
+++ lib/philomena_web/views/api/json/image_view.ex
@@ -17,67 +17,75 @@ defmodule PhilomenaWeb.Api.Json.ImageView do
     }
   end
 
-  def render("image.json", %{image: %{hidden_from_users: true} = image})
-      when is_nil(image.duplicate_id) do
-    %{
-      id: image.id,
-      created_at: image.created_at,
-      updated_at: image.updated_at,
-      first_seen_at: image.first_seen_at,
-      deletion_reason: image.deletion_reason,
-      duplicate_of: nil,
-      hidden_from_users: true
-    }
-  end
+  def render("image.json", %{image: image} = assigns) do
+    user =
+      case assigns do
+        %{conn: %{assigns: %{current_user: current_user}}} -> current_user
+        _ -> nil
+      end
 
-  def render("image.json", %{conn: conn, image: image}) do
-    result = render_one(image, PhilomenaWeb.Api.Json.ImageView, "image.json", %{image: image})
+    case Canada.Can.can?(user, :show, image) do
+      true ->
+        %{
+          id: image.id,
+          created_at: image.created_at,
+          updated_at: image.updated_at,
+          first_seen_at: image.first_seen_at,
+          width: image.image_width,
+          height: image.image_height,
+          mime_type: image.image_mime_type,
+          size: image.image_size,
+          orig_size: image.image_orig_size,
+          duration: image.image_duration,
+          animated: image.image_is_animated,
+          format: image.image_format,
+          aspect_ratio: image.image_aspect_ratio,
+          name: image.image_name,
+          sha512_hash: image.image_sha512_hash,
+          orig_sha512_hash: image.image_orig_sha512_hash,
+          tags: Enum.map(image.tags, & &1.name),
+          tag_ids: Enum.map(image.tags, & &1.id),
+          uploader: if(!!image.user and !image.anonymous, do: image.user.name),
+          uploader_id: if(!!image.user and !image.anonymous, do: image.user.id),
+          wilson_score: Philomena.Images.SearchIndex.wilson_score(image),
+          intensities: intensities(image),
+          score: image.score,
+          upvotes: image.upvotes_count,
+          downvotes: image.downvotes_count,
+          faves: image.faves_count,
+          comment_count: image.comments_count,
+          tag_count: length(image.tags),
+          description: image.description,
+          source_url:
+            if(Enum.count(image.sources) > 0, do: Enum.at(image.sources, 0).source, else: ""),
+          source_urls: Enum.map(image.sources, & &1.source),
+          view_url: ImageView.pretty_url(image, true, false, false),
+          representations: ImageView.thumb_urls(image, true),
+          thumbnails_generated: image.thumbnails_generated,
+          processed: image.processed,
+          deletion_reason: nil,
+          duplicate_of: image.duplicate_id,
+          hidden_from_users: image.hidden_from_users
+        }
+        |> Map.put(
+          :spoilered,
+          case assigns do
+            %{conn: conn} -> ImageView.filter_or_spoiler_hits?(conn, image)
+            _ -> false
+          end
+        )
 
-    Map.put(result, :spoilered, ImageView.filter_or_spoiler_hits?(conn, image))
-  end
-
-  def render("image.json", %{image: image}) do
-    %{
-      id: image.id,
-      created_at: image.created_at,
-      updated_at: image.updated_at,
-      first_seen_at: image.first_seen_at,
-      width: image.image_width,
-      height: image.image_height,
-      mime_type: image.image_mime_type,
-      size: image.image_size,
-      orig_size: image.image_orig_size,
-      duration: image.image_duration,
-      animated: image.image_is_animated,
-      format: image.image_format,
-      aspect_ratio: image.image_aspect_ratio,
-      name: image.image_name,
-      sha512_hash: image.image_sha512_hash,
-      orig_sha512_hash: image.image_orig_sha512_hash,
-      tags: Enum.map(image.tags, & &1.name),
-      tag_ids: Enum.map(image.tags, & &1.id),
-      uploader: if(!!image.user and !image.anonymous, do: image.user.name),
-      uploader_id: if(!!image.user and !image.anonymous, do: image.user.id),
-      wilson_score: Philomena.Images.SearchIndex.wilson_score(image),
-      intensities: intensities(image),
-      score: image.score,
-      upvotes: image.upvotes_count,
-      downvotes: image.downvotes_count,
-      faves: image.faves_count,
-      comment_count: image.comments_count,
-      tag_count: length(image.tags),
-      description: image.description,
-      source_url:
-        if(Enum.count(image.sources) > 0, do: Enum.at(image.sources, 0).source, else: ""),
-      source_urls: Enum.map(image.sources, & &1.source),
-      view_url: ImageView.pretty_url(image, true, false, false),
-      representations: ImageView.thumb_urls(image, true),
-      thumbnails_generated: image.thumbnails_generated,
-      processed: image.processed,
-      deletion_reason: nil,
-      duplicate_of: image.duplicate_id,
-      hidden_from_users: image.hidden_from_users
-    }
+      false ->
+        %{
+          id: image.id,
+          created_at: image.created_at,
+          updated_at: image.updated_at,
+          first_seen_at: image.first_seen_at,
+          deletion_reason: image.deletion_reason,
+          duplicate_of: image.duplicate_id,
+          hidden_from_users: image.hidden_from_users
+        }
+    end
   end
 
   def render("error.json", %{changeset: changeset}) do
