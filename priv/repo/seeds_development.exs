ae26e2d5806fd3f2eec4580e71ee09bff3bb1ccc
MareStare <mare.stare.official@gmail.com>
2025-03-27 23:47:28 +0000

    Fix script name in comment

--- priv/repo/seeds_development.exs
+++ priv/repo/seeds_development.exs
@@ -1,6 +1,6 @@
 # Script for populating the database. You can run it as:
 #
-#     mix run priv/repo/seeds.exs
+#     mix run priv/repo/seeds_development.exs
 #
 # Inside the script, you can read and write to any of your
 # repositories directly:
