643f756ad598fd3e03639071a71984649777ab67
MareStare <mare.stare.official@gmail.com>
2025-04-06 04:25:21 +0000

    Add `sh` file checks and fix lints

--- scripts/shellcheck.sh
+++ scripts/shellcheck.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+#
+# This script runs shellcheck on all shell scripts in the repository.
+# It exists because shellcheck doesn't attempt to automatically discover
+# shell scripts and requires specifying the files paths explicitly.
+#
+# This is somewhat understandable, because not all shell scripts use
+# obvious file extensions like `.sh`. So, we discover such files by
+# checking if they have one of the expected shebang lines at the beginning.
 
 set -euo pipefail
 
@@ -7,6 +15,7 @@ set -euo pipefail
 function shell_script_files {
   git ls-files "*.sh"
   git grep --files-with-matches '^#!/usr/bin/env bash'
+  git grep --files-with-matches '^#!/usr/bin/env sh'
 }
 
 mapfile -t files < <(shell_script_files "$@" | sort --unique)
