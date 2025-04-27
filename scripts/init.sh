03cea2e9df722741a8bc90bda95ef7801fc11157
MareStare <mare.stare.official@gmail.com>
2025-04-27 20:13:59 +0000

    Fix pre-commit hook failing on CI

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,5 +5,9 @@ set -euo pipefail
 
 . "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
 
-# Install the pre-commit hook. It's a symlink, to make sure it stays always up-to-date.
-step ln -sf ../../.githooks/pre-commit .git/hooks/pre-commit
+# No need for a pre-commit hook on CI. Moreover it fails because our CI runs
+# on behalf of the `root` user, and thus .git is root-owned.
+if [[ ! -v CI ]]; then
+    # Install the pre-commit hook. It's a symlink, to make sure it stays always up-to-date.
+    step ln -sf ../../.githooks/pre-commit .git/hooks/pre-commit
+fi
