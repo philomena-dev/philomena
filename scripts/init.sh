e7760dd4782a5b94b842e2090c70193c4260e4e7
MareStare <mare.stare.official@gmail.com>
2025-04-27 20:27:48 +0000

    Chow the entire current dir

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,7 +9,7 @@ set -euo pipefail
 # Also, CI runs on behalf of the `root` user, and thus .git is root-owned,
 # so we need to fix this, otherwise git commands report "dubious ownership".
 if [[ "${CI:-}" == "true" ]]; then
-    sudo chown -R "$(id -u):$(id -g)" .git
+    step sudo chown -R "$(id -u):$(id -g)" .
 else
     # Install the pre-commit hook. It's a symlink, to make sure it stays always up-to-date.
     step ln -sf ../../.githooks/pre-commit .git/hooks/pre-commit
