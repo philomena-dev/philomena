54f16e3061353a96e237e2327fcc9bd18df96b39
MareStare <mare.stare.official@gmail.com>
2025-04-27 20:25:22 +0000

    Fix?

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,9 +5,12 @@ set -euo pipefail
 
 . "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
 
-# No need for a pre-commit hook on CI. Moreover it fails because our CI runs
-# on behalf of the `root` user, and thus .git is root-owned.
-if [[ "${CI:-}" != "true" ]]; then
+# No need for a pre-commit hook on CI.
+# Also, CI runs on behalf of the `root` user, and thus .git is root-owned,
+# so we need to fix this, otherwise git commands report "dubious ownership".
+if [[ "${CI:-}" == "true" ]]; then
+    chown -R "$(id -u):$(id -g)" .git
+else
     # Install the pre-commit hook. It's a symlink, to make sure it stays always up-to-date.
     step ln -sf ../../.githooks/pre-commit .git/hooks/pre-commit
 fi
