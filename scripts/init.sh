c5d793d5ce8f0cccb640c67b172b27b9c231a9b2
MareStare <mare.stare.official@gmail.com>
2025-04-28 01:38:14 +0000

    Simplify env vars forwarding

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 # No need for a pre-commit hook on CI.
 # Also, CI runs on behalf of the `root` user, and thus .git is root-owned,
 # so we need to fix this, otherwise git commands report "dubious ownership".
-if [[ "${CI:-}" == "true" ]]; then
+if [[ -v CI ]]; then
     step sudo chown -R "$(id -u):$(id -g)" .
 else
     # Install the pre-commit hook. It's a symlink, to make sure it stays always up-to-date.
