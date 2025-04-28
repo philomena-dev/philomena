26d520237c5bd4e1a9aad8b4f300ab8d65b35a23
mdashlw <mdashlw@gmail.com>
2025-04-28 20:39:26 +0800

    Use new filters in find_page

--- lib/philomena_web/comment_loader.ex
+++ lib/philomena_web/comment_loader.ex
@@ -6,33 +6,21 @@ defmodule PhilomenaWeb.CommentLoader do
 
   def load_comments(conn, image) do
     user = conn.assigns.current_user
-    pref = load_direction(user)
-
     show_hidden? = staff?(user)
+    direction = load_direction(user)
 
     Comment
     |> where(image_id: ^image.id)
     |> filter_deleted(show_hidden?)
     |> filter_non_approved(user, show_hidden?)
-    |> order_by([{^pref, :created_at}])
+    |> order_by([{^direction, :created_at}])
     |> preload([:image, :deleted_by, user: [awards: :badge]])
     |> Repo.paginate(conn.assigns.comment_scrivener)
   end
 
-  def filter_deleted(query, true), do: query
-  def filter_deleted(query, _show_hidden?), do: where(query, [c], not c.destroyed_content)
-
-  def filter_non_approved(query, _user, true), do: query
-
-  def filter_non_approved(query, %{id: user_id}, _show_hidden?),
-    do: where(query, [c], c.approved or c.user_id == ^user_id)
-
-  def filter_non_approved(query, _user, _show_hidden?),
-    do: where(query, [c], c.approved)
-
-  # TODO new `where` filters fuck this
   def find_page(conn, image, comment_id) do
     user = conn.assigns.current_user
+    show_hidden? = staff?(user)
 
     comment =
       Comment
@@ -43,6 +31,8 @@ defmodule PhilomenaWeb.CommentLoader do
     offset =
       Comment
       |> where(image_id: ^image.id)
+      |> filter_deleted(show_hidden?)
+      |> filter_non_approved(user, show_hidden?)
       |> filter_direction(comment.created_at, user)
       |> Repo.aggregate(:count, :id)
 
@@ -64,8 +54,22 @@ defmodule PhilomenaWeb.CommentLoader do
     div(offset, page_size) + 1
   end
 
+  defp staff?(%{role: role}) when role in ~W(assistant moderator admin), do: true
+  defp staff?(_user), do: false
+
   defp load_direction(%{comments_newest_first: false}), do: :asc
   defp load_direction(_user), do: :desc
+
+  defp filter_deleted(query, true), do: query
+  defp filter_deleted(query, _show_hidden?), do: where(query, [c], not c.destroyed_content)
+
+  defp filter_non_approved(query, _user, true), do: query
+
+  defp filter_non_approved(query, %{id: user_id}, _show_hidden?),
+    do: where(query, [c], c.approved or c.user_id == ^user_id)
+
+  defp filter_non_approved(query, _user, _show_hidden?),
+    do: where(query, [c], c.approved)
 
   defp filter_direction(query, time, %{comments_newest_first: false}),
     do: where(query, [c], c.created_at <= ^time)
@@ -103,9 +107,6 @@ defmodule PhilomenaWeb.CommentLoader do
     |> hide_deleted(show_hidden?)
     |> hide_non_approved(user, show_hidden?)
   end
-
-  defp staff?(%{role: role}) when role in ~W(assistant moderator admin), do: true
-  defp staff?(_user), do: false
 
   defp hide_deleted(filters, true), do: filters
 
