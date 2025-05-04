03271373835859298f9fb654618a2528df342945
mdashlw <mdashlw@gmail.com>
2025-05-04 19:58:17 +0800

    Make list a const

--- lib/philomena_web/user_loader.ex
+++ lib/philomena_web/user_loader.ex
@@ -2,6 +2,17 @@ defmodule PhilomenaWeb.UserLoader do
   alias PhilomenaQuery.Search
   alias Philomena.Users.User
 
+  @sortable_fields ~W(
+    uploads_count
+    images_favourited_count
+    comments_posted_count
+    votes_cast_count
+    metadata_updates_count
+    forum_posts_count
+    topic_count
+    _score
+  )
+
   def query(conn, body, options \\ []) do
     pagination = Keyword.get(options, :pagination, conn.assigns.pagination)
     sort = Keyword.get(options, :sort) || parse_sort(conn.params)
@@ -22,9 +33,8 @@ defmodule PhilomenaWeb.UserLoader do
   defp parse_sd(%{"usd" => sd}) when sd in ~W(asc desc), do: sd
   defp parse_sd(_params), do: "desc"
 
-  defp parse_sf(%{"usf" => sf}, sd)
-       when sf in ~W(uploads_count images_favourited_count comments_posted_count votes_cast_count metadata_updates_count forum_posts_count topic_count _score),
-       do: [%{sf => sd}, %{"id" => sd}]
+  defp parse_sf(%{"usf" => sf}, sd) when sf in @sortable_fields,
+    do: [%{sf => sd}, %{"id" => sd}]
 
   defp parse_sf(_params, sd),
     do: [%{"id" => sd}]
