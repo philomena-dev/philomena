062be42c03da6402148f9a00efc58773fe679357
MareStare <mare.stare.official@gmail.com>
2025-05-14 03:27:43 +0000

    Add a comment about refactoring in philomena.sh

--- scripts/philomena.sh
+++ scripts/philomena.sh
@@ -103,6 +103,8 @@ function init {
 
 # Update the `queries.json` test snapshots after the implementation changes.
 function update_search_syntax_tests {
+  # TODO: refactor this after the devcontainer PR is merged:
+  # https://github.com/philomena-dev/philomena/pull/528
   # shellcheck disable=SC2016
   test='
     set -euo pipefail
@@ -110,6 +112,8 @@ function update_search_syntax_tests {
     . scripts/lib.sh
 
     export MIX_ENV=test
+
+    step mix deps.get
 
     if step createdb -h postgres -U postgres philomena_test; then
       step mix ecto.create
@@ -119,7 +123,7 @@ function update_search_syntax_tests {
     fi
 
     step mix test test/philomena/images/query_test.exs
-    step npx prettier --write test/philomena/images/search-syntax.json
+    step npx prettier --write test/philomena/images/search-syntax.yml
   '
 
   ASSERT_VALUE_ACCEPT_DIFFS=y step docker compose run \
