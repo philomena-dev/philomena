85090a088ae3ef77f1f5ef3aaa782eeffadf4b3b
Luna D. <luna@nighty.cloud>
2025-09-22 12:09:38 +0200

    also use "magick identify"

--- lib/philomena_media/processors/jpeg.ex
+++ lib/philomena_media/processors/jpeg.ex
@@ -42,7 +42,7 @@ defmodule PhilomenaMedia.Processors.Jpeg do
 
   defp requires_lossy_transformation?(file) do
     with {output, 0} <-
-           System.cmd("identify", ["-format", "%[orientation]\t%[profile:icc]", file]),
+           System.cmd("magick", ["identify", "-format", "%[orientation]\t%[profile:icc]", file]),
          [orientation, profile] <- String.split(output, "\t") do
       orientation not in ["Undefined", "TopLeft"] or profile != ""
     else
