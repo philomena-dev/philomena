43df9a139fcf030d8dbd6942f1a252735126603a
Luna D. <luna@nighty.cloud>
2025-10-28 17:30:55 +0100

    use more appropriate words as opposed to "thing"

--- lib/philomena_web/views/admin/mod_note_view.ex
+++ lib/philomena_web/views/admin/mod_note_view.ex
@@ -5,20 +5,20 @@ defmodule PhilomenaWeb.Admin.ModNoteView do
   alias Philomena.Reports.Report
   alias Philomena.DnpEntries.DnpEntry
 
-  def link_to_noted_thing(%DnpEntry{tag: tag} = dnp_entry),
+  def link_to_notable(%DnpEntry{tag: tag} = dnp_entry),
     do: link("DNP entry for #{tag.name}", to: ~p"/dnp/#{dnp_entry}")
 
-  def link_to_noted_thing(%Report{user: nil} = report),
+  def link_to_notable(%Report{user: nil} = report),
     do: link("Report #{report.id}", to: ~p"/admin/reports/#{report}")
 
-  def link_to_noted_thing(%Report{user: user} = report),
+  def link_to_notable(%Report{user: user} = report),
     do:
       link("Report #{report.id} by #{user.name}",
         to: ~p"/admin/reports/#{report}"
       )
 
-  def link_to_noted_thing(%User{} = user),
+  def link_to_notable(%User{} = user),
     do: link("User #{user.name}", to: ~p"/profiles/#{user}")
 
-  def link_to_noted_thing(_notable), do: "Item permanently deleted"
+  def link_to_notable(_notable), do: "Item permanently deleted"
 end
