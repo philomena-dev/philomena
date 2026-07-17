fba398dbee03b68a35aea27cb485383a7b395a90
Luna Fox <luna@nighty.cloud>
2026-07-17 10:22:00 +0200

    shorten version attribution

--- lib/philomena/versions/attribution.ex
+++ lib/philomena/versions/attribution.ex
@@ -1,21 +1,5 @@
-defimpl Philomena.Attribution, for: Philomena.Posts.PostVersion do
-  def object_identifier(version) do
-    Philomena.Attribution.object_identifier(version.parent)
-  end
-
-  def best_user_identifier(version) do
-    Philomena.Attribution.best_user_identifier(version.parent)
-  end
-
-  def anonymous?(version) do
-    same_user?(version.user, version.parent) and !!version.parent.anonymous
-  end
-
-  defp same_user?(%{id: id}, %{user_id: id}), do: true
-  defp same_user?(_user, _parent), do: false
-end
-
-defimpl Philomena.Attribution, for: Philomena.Comments.CommentVersion do
+defimpl Philomena.Attribution,
+  for: [Philomena.Comments.CommentVersion, Philomena.Posts.PostVersion] do
   def object_identifier(version) do
     Philomena.Attribution.object_identifier(version.parent)
   end
